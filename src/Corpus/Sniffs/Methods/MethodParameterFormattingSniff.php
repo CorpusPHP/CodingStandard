0422497853fa36708ab5e0854d4046868fbfd35f
Jesse Donat <donatj@gmail.com>
2020-06-28 15:33:01 -0500

    Adds rough MethodParameterFormattingSniff

---
+++ src/Corpus/Sniffs/Methods/MethodParameterFormattingSniff.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Corpus\Sniffs\Methods;
+
+use PHP_CodeSniffer\Files\File;
+use PHP_CodeSniffer\Sniffs\Sniff;
+
+class MethodParameterFormattingSniff implements Sniff {
+
+	public $maxLength = 130;
+
+	public const CODE_OVERLY_LONG_ARGUMENT_LIST = 'OverlyLongArgumentList';
+
+	public function register() {
+		return [ T_FUNCTION ];
+	}
+
+	public function process( File $phpcsFile, $stackPtr ) {
+		$tokens = $phpcsFile->getTokens();
+
+		$scopePtr = $tokens[$stackPtr]['scope_opener'] ?? $phpcsFile->findEndOfStatement($stackPtr);
+
+		if( $tokens[$stackPtr]['line'] !== $tokens[$scopePtr]['line'] ) {
+			return;
+		}
+
+		if( $tokens[$scopePtr]['column'] < $this->maxLength ) {
+			return;
+		}
+
+		$stmtStartPtr = $phpcsFile->findStartOfStatement($stackPtr);
+		$indent       = str_repeat("\t", $tokens[$stmtStartPtr]['column']);
+
+		$fix = $phpcsFile->addFixableError(
+			"Single line function definition exceeds %d characters",
+			$stackPtr,
+			self::CODE_OVERLY_LONG_ARGUMENT_LIST,
+			[ $this->maxLength ]
+		);
+
+		if( $fix === true ) {
+			$startPtr = $tokens[$stackPtr]['parenthesis_opener'];
+			$endPtr   = $tokens[$stackPtr]['parenthesis_closer'];
+			$phpcsFile->fixer->beginChangeset();
+
+			if( $tokens[$startPtr + 1]['code'] === T_WHITESPACE ) {
+				$phpcsFile->fixer->replaceToken($startPtr + 1, '');
+			}
+
+			$phpcsFile->fixer->addContent($startPtr, $phpcsFile->eolChar . $indent);
+
+			for( $ptr = $stackPtr + 1; $ptr < $endPtr; $ptr++ ) {
+				if( $tokens[$ptr]['code'] === T_COMMA ) {
+					if( $tokens[$ptr + 1]['code'] === T_WHITESPACE ) {
+						$phpcsFile->fixer->replaceToken($ptr + 1, '');
+					}
+
+					$phpcsFile->fixer->addContent($ptr, $phpcsFile->eolChar . $indent);
+				}
+			}
+
+
+			$phpcsFile->fixer->addContentBefore(
+				$endPtr,
+				$phpcsFile->eolChar . str_repeat("\t", $tokens[$stmtStartPtr]['column'] - 1)
+			);
+
+			$phpcsFile->fixer->endChangeset();
+		}
+	}
+
+}
